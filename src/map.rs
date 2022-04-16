7da7aa1c3df7e370524dbd24baf9a1f182ff4e68
Simon Bergot <sbergot@cegid.com>
2022-04-16 16:55:08 +0200

    use weighted random table

--- src/map.rs
+++ src/map.rs
@@ -195,6 +195,42 @@ impl Map {
         }
     }
 
+    fn wall_glyph(&self, pos: Point) -> FontCharType {
+        if pos.x < 1 || pos.x > self.width-2 || pos.y < 1 || pos.y > self.height-2 { return 35; }
+        let mut mask : u8 = 0;
+
+        if self.is_revealed_and_wall(pos + Point {x: 0, y:-1}) { mask +=1; }
+        if self.is_revealed_and_wall(pos + Point {x: 0, y: 1}) { mask +=2; }
+        if self.is_revealed_and_wall(pos + Point {x:-1, y: 0}) { mask +=4; }
+        if self.is_revealed_and_wall(pos + Point {x: 1, y: 0}) { mask +=8; }
+    
+        match mask {
+            0 => { 9 } // Pillar because we can't see neighbors
+            1 => { 186 } // Wall only to the north
+            2 => { 186 } // Wall only to the south
+            3 => { 186 } // Wall to the north and south
+            4 => { 205 } // Wall only to the west
+            5 => { 188 } // Wall to the north and west
+            6 => { 187 } // Wall to the south and west
+            7 => { 185 } // Wall to the north, south and west
+            8 => { 205 } // Wall only to the east
+            9 => { 200 } // Wall to the north and east
+            10 => { 201 } // Wall to the south and east
+            11 => { 204 } // Wall to the north, south and east
+            12 => { 205 } // Wall to the east and west
+            13 => { 202 } // Wall to the east, west, and south
+            14 => { 203 } // Wall to the east, west, and north
+            15 => { 206 }  // ╬ Wall on all sides
+            _ => { 35 } // We missed one?
+        }
+    }
+    
+    fn is_revealed_and_wall(&self, pos: Point) -> bool {
+        let idx = self.xy_idx(pos);
+        self.tiles[idx] == TileType::Wall && self.revealed_tiles[idx]
+    }
+    
+
     pub fn draw_map(ecs: &World, ctx: &mut BTerm) {
         let map = ecs.fetch::<Map>();
 
@@ -210,7 +246,7 @@ impl Map {
                         fg = RGB::from_f32(0.0, 0.5, 0.5);
                     }
                     TileType::Wall => {
-                        glyph = to_cp437('#');
+                        glyph = map.wall_glyph(map.idx_xy(idx));
                         fg = RGB::from_f32(0., 1.0, 0.);
                     }
                 }
--- src/spawner.rs
+++ src/spawner.rs
@@ -1,9 +1,10 @@
+use std::collections::HashMap;
+
 use crate::components::*;
 use bracket_lib::prelude::*;
-use specs::{*, saveload::*};
+use specs::{saveload::*, *};
 
 const MAX_MONSTERS: i32 = 4;
-const MAX_ITEMS: i32 = 5;
 
 pub fn player(ecs: &mut World, pos: Point) -> Entity {
     ecs.create_entity()
@@ -27,18 +28,6 @@ pub fn player(ecs: &mut World, pos: Point) -> Entity {
         })
         .marked::<SimpleMarker<SerializeMe>>()
         .build()
-}
-
-pub fn random_monster(ecs: &mut World, pos: Point) {
-    let roll: i32;
-    {
-        let mut rng = ecs.write_resource::<RandomNumberGenerator>();
-        roll = rng.roll_dice(1, 2);
-    }
-    match roll {
-        1 => orc(ecs, pos),
-        _ => goblin(ecs, pos),
-    }
 }
 
 fn orc(ecs: &mut World, pos: Point) {
@@ -78,50 +67,99 @@ fn monster<S: ToString>(ecs: &mut World, pos: Point, glyph: FontCharType, name: 
 }
 
 pub fn spawn_room(ecs: &mut World, room: &Rect) {
-    let mut monster_spawn_points: Vec<Point> = Vec::new();
-    let mut item_spawn_points: Vec<Point> = Vec::new();
+    let spawn_table = room_table();
+    let mut spawn_points: HashMap<Point, ()> = HashMap::new();
 
     // Scope to keep the borrow checker happy
     {
         let mut rng = ecs.write_resource::<RandomNumberGenerator>();
-        let num_monsters = rng.roll_dice(1, MAX_MONSTERS + 2) - 3;
-        let num_items = rng.roll_dice(1, MAX_ITEMS + 2) - 3;
+        let num_spawns = rng.roll_dice(1, MAX_MONSTERS + 3) - 3;
 
-        for _i in 0..num_monsters {
+        for _i in 0..num_spawns {
             let mut added = false;
-            while !added {
+            let mut tries = 0;
+            while !added && tries < 20 {
                 let x = room.x1 + rng.roll_dice(1, i32::abs(room.x2 - room.x1));
                 let y = room.y1 + rng.roll_dice(1, i32::abs(room.y2 - room.y1));
                 let pos = Point { x, y };
-                if !monster_spawn_points.contains(&pos) {
-                    monster_spawn_points.push(pos);
+                if !spawn_points.contains_key(&pos) {
+                    spawn_points.insert(pos, ());
                     added = true;
-                }
-            }
-        }
-
-        for _i in 0..num_items {
-            let mut added = false;
-            while !added {
-                let x = room.x1 + rng.roll_dice(1, i32::abs(room.x2 - room.x1));
-                let y = room.y1 + rng.roll_dice(1, i32::abs(room.y2 - room.y1));
-                let pos = Point { x, y };
-                if !item_spawn_points.contains(&pos) {
-                    item_spawn_points.push(pos);
-                    added = true;
+                } else {
+                    tries += 1;
                 }
             }
         }
     }
 
-    // Actually spawn the monsters
-    for pos in monster_spawn_points.iter() {
-        random_monster(ecs, *pos);
+    {
+        for (spawn_pos, _u) in spawn_points.iter() {
+            spawn_table.roll(ecs, *spawn_pos);
+        }
+    }
+}
+
+pub struct RandomEntry {
+    spawn_fn: fn(ecs: &mut World, pos: Point),
+    weight: i32,
+}
+
+pub struct RandomTable {
+    entries: Vec<RandomEntry>,
+    total_weight: i32,
+}
+
+impl RandomTable {
+    pub fn new() -> RandomTable {
+        RandomTable {
+            entries: Vec::new(),
+            total_weight: 0,
+        }
     }
 
-    for pos in item_spawn_points.iter() {
-        random_item(ecs, *pos);
+    pub fn add(mut self, spawn_fn: fn(ecs: &mut World, pos: Point), weight: i32) -> RandomTable {
+        self.total_weight += weight;
+        self.entries.push(RandomEntry { spawn_fn, weight });
+        self
     }
+
+    pub fn roll(&self, ecs: &mut World, pos: Point) {
+        if self.total_weight == 0 {
+            return;
+        }
+
+        let mut roll;
+        {
+            let mut rng = ecs.write_resource::<RandomNumberGenerator>();
+            roll = rng.roll_dice(1, self.total_weight) - 1;
+        }
+
+        let mut index: usize = 0;
+        while roll > 0 {
+            let entry = &self.entries[index];
+            if roll < entry.weight {
+                let spawner = entry.spawn_fn;
+                spawner(ecs, pos);
+            }
+
+            roll -= entry.weight;
+            index += 1;
+        }
+    }
+}
+
+fn room_table() -> RandomTable {
+    RandomTable::new()
+        .add(goblin, 10)
+        .add(orc, 1)
+        .add(health_potion, 7)
+        .add(fireball_scroll, 2)
+        .add(confusion_scroll, 2)
+        .add(magic_missile_scroll, 4)
+        .add(dagger, 3)
+        .add(shield, 3)
+        .add(longsword, 2)
+        .add(tower_shield, 2)
 }
 
 fn health_potion(ecs: &mut World, pos: Point) {
@@ -165,87 +203,125 @@ fn magic_missile_scroll(ecs: &mut World, pos: Point) {
 
 fn fireball_scroll(ecs: &mut World, pos: Point) {
     ecs.create_entity()
-        .with(Position{ pos })
-        .with(Renderable{
+        .with(Position { pos })
+        .with(Renderable {
             glyph: to_cp437(')'),
             fg: RGB::named(ORANGE),
             bg: RGB::named(BLACK),
-            render_order: 2
+            render_order: 2,
         })
-        .with(Name{ name : "Fireball Scroll".to_string() })
-        .with(Item{})
-        .with(Consumable{})
-        .with(Ranged{ range: 6 })
-        .with(InflictsDamage{ damage: 20 })
-        .with(AreaOfEffect{ radius: 3 })
+        .with(Name {
+            name: "Fireball Scroll".to_string(),
+        })
+        .with(Item {})
+        .with(Consumable {})
+        .with(Ranged { range: 6 })
+        .with(InflictsDamage { damage: 20 })
+        .with(AreaOfEffect { radius: 3 })
         .marked::<SimpleMarker<SerializeMe>>()
         .build();
 }
 
 fn confusion_scroll(ecs: &mut World, pos: Point) {
     ecs.create_entity()
-        .with(Position{ pos })
-        .with(Renderable{
+        .with(Position { pos })
+        .with(Renderable {
             glyph: to_cp437(')'),
             fg: RGB::named(PINK),
             bg: RGB::named(BLACK),
-            render_order: 2
+            render_order: 2,
         })
-        .with(Name{ name : "Confusion Scroll".to_string() })
-        .with(Item{})
-        .with(Consumable{})
-        .with(Ranged{ range: 6 })
-        .with(Confusion{ turns: 4 })
+        .with(Name {
+            name: "Confusion Scroll".to_string(),
+        })
+        .with(Item {})
+        .with(Consumable {})
+        .with(Ranged { range: 6 })
+        .with(Confusion { turns: 4 })
         .marked::<SimpleMarker<SerializeMe>>()
         .build();
 }
 
 fn dagger(ecs: &mut World, pos: Point) {
     ecs.create_entity()
-        .with(Position{ pos })
-        .with(Renderable{
+        .with(Position { pos })
+        .with(Renderable {
             glyph: to_cp437('/'),
             fg: RGB::named(CYAN),
             bg: RGB::named(BLACK),
-            render_order: 2
+            render_order: 2,
         })
-        .with(Name{ name : "Dagger".to_string() })
-        .with(Item{})
-        .with(Equippable{ slot: EquipmentSlot::Melee })
-        .with(MeleePowerBonus{ power: 2 })
+        .with(Name {
+            name: "Dagger".to_string(),
+        })
+        .with(Item {})
+        .with(Equippable {
+            slot: EquipmentSlot::Melee,
+        })
+        .with(MeleePowerBonus { power: 2 })
         .marked::<SimpleMarker<SerializeMe>>()
         .build();
 }
 
 fn shield(ecs: &mut World, pos: Point) {
     ecs.create_entity()
-        .with(Position{ pos })
-        .with(Renderable{
+        .with(Position { pos })
+        .with(Renderable {
             glyph: to_cp437('('),
             fg: RGB::named(CYAN),
             bg: RGB::named(BLACK),
-            render_order: 2
+            render_order: 2,
         })
-        .with(Name{ name : "Shield".to_string() })
-        .with(Item{})
-        .with(Equippable{ slot: EquipmentSlot::Shield })
-        .with(DefenseBonus{ defense: 1 })
+        .with(Name {
+            name: "Shield".to_string(),
+        })
+        .with(Item {})
+        .with(Equippable {
+            slot: EquipmentSlot::Shield,
+        })
+        .with(DefenseBonus { defense: 1 })
         .marked::<SimpleMarker<SerializeMe>>()
         .build();
 }
 
-fn random_item(ecs: &mut World, pos: Point) {
-    let roll: i32;
-    {
-        let mut rng = ecs.write_resource::<RandomNumberGenerator>();
-        roll = rng.roll_dice(1, 6);
-    }
-    match roll {
-        1 => health_potion(ecs, pos),
-        2 => fireball_scroll(ecs, pos),
-        3 => confusion_scroll(ecs, pos),
-        4 => dagger(ecs, pos),
-        5 => shield(ecs, pos),
-        _ => magic_missile_scroll(ecs, pos),
-    }
+fn longsword(ecs: &mut World, pos: Point) {
+    ecs.create_entity()
+        .with(Position { pos })
+        .with(Renderable {
+            glyph: to_cp437('/'),
+            fg: RGB::named(YELLOW),
+            bg: RGB::named(BLACK),
+            render_order: 2,
+        })
+        .with(Name {
+            name: "Longsword".to_string(),
+        })
+        .with(Item {})
+        .with(Equippable {
+            slot: EquipmentSlot::Melee,
+        })
+        .with(MeleePowerBonus { power: 4 })
+        .marked::<SimpleMarker<SerializeMe>>()
+        .build();
+}
+
+fn tower_shield(ecs: &mut World, pos: Point) {
+    ecs.create_entity()
+        .with(Position { pos })
+        .with(Renderable {
+            glyph: to_cp437('('),
+            fg: RGB::named(YELLOW),
+            bg: RGB::named(BLACK),
+            render_order: 2,
+        })
+        .with(Name {
+            name: "Tower Shield".to_string(),
+        })
+        .with(Item {})
+        .with(Equippable {
+            slot: EquipmentSlot::Shield,
+        })
+        .with(DefenseBonus { defense: 3 })
+        .marked::<SimpleMarker<SerializeMe>>()
+        .build();
 }
