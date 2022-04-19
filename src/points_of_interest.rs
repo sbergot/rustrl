3b081ca93b87a42b3beeadfa70a6f64935249247
Simon Bergot <sbergot@cegid.com>
2022-04-19 14:19:05 +0200

    fix poi bug

--- src/points_of_interest.rs
+++ src/points_of_interest.rs
@@ -18,7 +18,7 @@ impl PointsOfInterest {
     pub fn get_next(&self, prev: Point) -> Option<Point> {
         for (idx, poi) in self.0.iter().enumerate() {
             if *poi == prev {
-                return Some(self.0[idx + 1 % self.0.len()]);
+                return Some(self.0[(idx + 1) % self.0.len()]);
             }
         }
         return if self.0.is_empty() { None } else { Some(self.0[0]) };
