537b944446ca4ef6aaf47560087e8ebf02df0016
Simon Bergot <simon.bergot@gmail.com>
2022-04-23 08:12:54 +0200

    add decal system

--- src/map.rs
+++ src/map.rs
@@ -1,7 +1,10 @@
 use bracket_lib::prelude::*;
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 use specs::*;
-use std::cmp::{max, min};
+use std::{
+    cmp::{max, min},
+    collections::HashMap,
+};
 
 use crate::points_of_interest::PointsOfInterest;
 
@@ -11,12 +14,26 @@ pub enum TileType {
     Floor,
 }
 
+#[derive(PartialEq, Copy, Clone, Serialize, Deserialize)]
+pub struct Decal {
+    color: RGB,
+}
+
+impl Decal {
+    pub fn blood() -> Decal {
+        Decal {
+            color: RGB::named(RED),
+        }
+    }
+}
+
 #[derive(Default, Serialize, Deserialize, Clone)]
 pub struct Map {
     pub tiles: Vec<TileType>,
     pub revealed_tiles: Vec<bool>,
     pub visible_tiles: Vec<bool>,
     pub blocked_tiles: Vec<bool>,
+    pub decal_tiles: HashMap<Point, Decal>,
 
     #[serde(skip_serializing)]
     #[serde(skip_deserializing)]
@@ -115,6 +132,7 @@ impl Map {
             visible_tiles: vec![false; max_idx],
             blocked_tiles: vec![false; max_idx],
             entities_tiles: vec![Vec::new(); max_idx],
+            decal_tiles: HashMap::new(),
             rooms: Vec::new(),
             width: width,
             height: height,
@@ -198,40 +216,49 @@ impl Map {
     }
 
     fn wall_glyph(&self, pos: Point) -> FontCharType {
-        if pos.x < 1 || pos.x > self.width-2 || pos.y < 1 || pos.y > self.height-2 { return 35; }
-        let mut mask : u8 = 0;
+        if pos.x < 1 || pos.x > self.width - 2 || pos.y < 1 || pos.y > self.height - 2 {
+            return 35;
+        }
+        let mut mask: u8 = 0;
 
-        if self.is_revealed_and_wall(pos + Point {x: 0, y:-1}) { mask +=1; }
-        if self.is_revealed_and_wall(pos + Point {x: 0, y: 1}) { mask +=2; }
-        if self.is_revealed_and_wall(pos + Point {x:-1, y: 0}) { mask +=4; }
-        if self.is_revealed_and_wall(pos + Point {x: 1, y: 0}) { mask +=8; }
-    
+        if self.is_revealed_and_wall(pos + Point { x: 0, y: -1 }) {
+            mask += 1;
+        }
+        if self.is_revealed_and_wall(pos + Point { x: 0, y: 1 }) {
+            mask += 2;
+        }
+        if self.is_revealed_and_wall(pos + Point { x: -1, y: 0 }) {
+            mask += 4;
+        }
+        if self.is_revealed_and_wall(pos + Point { x: 1, y: 0 }) {
+            mask += 8;
+        }
+
         match mask {
-            0 => { 9 } // Pillar because we can't see neighbors
-            1 => { 186 } // Wall only to the north
-            2 => { 186 } // Wall only to the south
-            3 => { 186 } // Wall to the north and south
-            4 => { 205 } // Wall only to the west
-            5 => { 188 } // Wall to the north and west
-            6 => { 187 } // Wall to the south and west
-            7 => { 185 } // Wall to the north, south and west
-            8 => { 205 } // Wall only to the east
-            9 => { 200 } // Wall to the north and east
-            10 => { 201 } // Wall to the south and east
-            11 => { 204 } // Wall to the north, south and east
-            12 => { 205 } // Wall to the east and west
-            13 => { 202 } // Wall to the east, west, and south
-            14 => { 203 } // Wall to the east, west, and north
-            15 => { 206 }  // ╬ Wall on all sides
-            _ => { 35 } // We missed one?
+            0 => 9,    // Pillar because we can't see neighbors
+            1 => 186,  // Wall only to the north
+            2 => 186,  // Wall only to the south
+            3 => 186,  // Wall to the north and south
+            4 => 205,  // Wall only to the west
+            5 => 188,  // Wall to the north and west
+            6 => 187,  // Wall to the south and west
+            7 => 185,  // Wall to the north, south and west
+            8 => 205,  // Wall only to the east
+            9 => 200,  // Wall to the north and east
+            10 => 201, // Wall to the south and east
+            11 => 204, // Wall to the north, south and east
+            12 => 205, // Wall to the east and west
+            13 => 202, // Wall to the east, west, and south
+            14 => 203, // Wall to the east, west, and north
+            15 => 206, // ╬ Wall on all sides
+            _ => 35,   // We missed one?
         }
     }
-    
+
     fn is_revealed_and_wall(&self, pos: Point) -> bool {
         let idx = self.xy_idx(pos);
         self.tiles[idx] == TileType::Wall && self.revealed_tiles[idx]
     }
-    
 
     pub fn draw_map(ecs: &World, ctx: &mut BTerm) {
         let map = ecs.read_resource::<Map>();
@@ -243,13 +270,14 @@ impl Map {
             if map.revealed_tiles[idx] {
                 let glyph;
                 let mut fg;
+                let pos = map.idx_xy(idx);
                 match tile {
                     TileType::Floor => {
                         glyph = to_cp437('.');
                         fg = RGB::from_f32(0.0, 0.5, 0.5);
                     }
                     TileType::Wall => {
-                        glyph = map.wall_glyph(map.idx_xy(idx));
+                        glyph = map.wall_glyph(pos);
                         fg = RGB::from_f32(0., 1.0, 0.);
                     }
                 }
@@ -257,7 +285,13 @@ impl Map {
                     fg = fg.to_greyscale()
                 }
 
-                let bg = if poi.contains(map.idx_xy(idx)) { RGB::named(BLUE) } else { RGB::from_f32(0., 0., 0.) };
+                let bg = if poi.contains(pos) {
+                    RGB::named(BLUE)
+                } else if let Some(decal) = map.decal_tiles.get(&pos) {
+                    decal.color
+                } else {
+                    RGB::from_f32(0., 0., 0.)
+                };
                 ctx.set(x, y, fg, bg, glyph);
             }
 
--- src/systems/damage_system.rs
+++ src/systems/damage_system.rs
@@ -1,4 +1,4 @@
-use crate::components::*;
+use crate::{components::*, map::{Map, Decal}};
 use specs::prelude::*;
 
 pub struct DamageSystem {}
@@ -7,13 +7,16 @@ impl<'a> System<'a> for DamageSystem {
     type SystemData = (
         WriteStorage<'a, CombatStats>,
         WriteStorage<'a, SufferDamage>,
+        ReadStorage<'a, Position>,
+        WriteExpect<'a, Map>,
     );
 
     fn run(&mut self, data: Self::SystemData) {
-        let (mut stats, mut damage) = data;
+        let (mut stats, mut damage, positions, mut map) = data;
 
-        for (mut stats, damage) in (&mut stats, &damage).join() {
+        for (mut stats, damage, position) in (&mut stats, &damage, &positions).join() {
             stats.hp -= damage.amount.iter().sum::<i32>();
+            map.decal_tiles.insert(position.pos, Decal::blood());
         }
 
         damage.clear();
